8064e5a516ba5713a67d56fc6bef008dd09dc260
unknown <dawid.pios00@gmail.com>
2024-04-20 20:15:14 +0200

    Backend try pt.1

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,22 @@
+
 import { Hero } from "@component/components/component/hero";
 
 
-export default function Home() {
+const Home = async () => {
+
+  
+  const get = await fetch('http://localhost:8080/')
+  const res = await get.json()
+
+  console.log(res.message)
+ 
+
   return (
     <main className="sm: p-10 flex min-h-screen flex-col items-center justify-between lg:p-24">
       <Hero></Hero>
+      <p>{res.message}</p>
     </main>
   );
 }
+
+export default Home
